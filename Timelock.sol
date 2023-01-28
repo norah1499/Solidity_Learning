6546429e96a3e515c54aa45f330ad4b3ea23e197
rohan <121377774+rohangosar23@users.noreply.github.com>
2023-01-28 15:22:14 +0530

    Add files via upload

---
+++ Timelock.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+//This contract locks users fund for duration of week
+contract TimeLock{
+
+    //Mapping to store the amount of money deposited by user
+    mapping (address => uint) public deposits;
+
+    //Mappig to keep track of the time left until the user can withdraw
+    mapping (address => uint) public VestingTime;
+
+    //Function to deposit money
+    function deposit() public payable {
+        
+        //update the balance and lockIn time of the depositer
+        deposits[msg.sender] = msg.value;
+        VestingTime[msg.sender] = block.timestamp + 1 weeks;
+
+        //transfer money to contracts address
+        //payable(msg.sender).transfer(msg.value);
+       
+    }
+
+    //Function to withdraw deposited money
+    function withdraw() public {
+
+        //check if user hasdeposoted any money
+        require(deposits[msg.sender]>0,"user does not exist");
+
+        //check if the lock in time is over or not  
+        require(VestingTime[msg.sender] < block.timestamp, "Vesting period is not over yet");
+
+        //sending money from contracts address to user
+        (bool sent, ) = msg.sender.call{value : deposits[msg.sender]}("");
+
+        //checking if the transacton went succesfull or not
+        require(sent,"Failed to send ether");
+
+        //updating the balance of user
+        deposits[msg.sender] = 0;
+
+    }
+
+}
